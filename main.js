b64e27ab79ebd4cc78af70b31d8294f4c22a2ece
agostinopique <ago.pique@yahoo.it>
2022-04-07 16:31:51 +0200

    Fisrt commit; Aggiunti querySelector e classi da aggiungere e rimuovere;

---
+++ main.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger-menu');
+
+const menu = document.querySelector('.header-right > a');
+
+const cross = document.querySelector('.hamburger-menu > a');
+
+console.log(hamburger);
+console.log(menu);
+console.log(cross);
+
+menu.addEventListener('click', function(){
+    hamburger.classList.add('active');
+})
+
+cross.addEventListener('click', function(){
+    hamburger.classList.remove('active');
+})
